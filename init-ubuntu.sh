7031f197d879d6ce7ad15acebb10bb03d3e6e477
Jesse Reitz <jessereitz1@gmail.com>
2018-10-31 22:06:37 -0500

    Add chrome installation

--- init-ubuntu.sh
+++ init-ubuntu.sh
@@ -4,8 +4,12 @@
 add-apt-repository main
 add-apt-repository universe
 add-apt-repository multiverse
+# Chrome repository
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
 apt update
 apt upgrade
+sudo apt-get -y install google-chrome-stable
 
 # Install git
 sudo apt install git
