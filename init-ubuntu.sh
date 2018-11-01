46bc06e090ba288c8678a6c5dc87f58693e9870f
Jesse Reitz <jessereitz1@gmail.com>
2018-11-01 00:00:54 -0500

    Fix proper user process for gnome preference tweaking.

--- init-ubuntu.sh
+++ init-ubuntu.sh
@@ -22,8 +22,8 @@ git config --global user.email "jessereitz1@gmail.com"
 # Install Gnome-Tweaks and set up Gnome
 echo -e "\n\nInstalling Gnome-Tweaks. Setting up Gnome preferences."
 apt install --assume-yes gnome-tweak-tool
--H -u $SUDO_USER bash -c 'gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM'
--H -u $SUDO_USER bash -c 'gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true'
+su -c $SUDO_USER 'gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM'
+su -c $SUDO_USER 'gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true'
 
 
 # Install text editors and their configurations
