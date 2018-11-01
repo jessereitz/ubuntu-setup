5071a6bdfefe7a58440c9187f68e6a00a6fa03c7
Jesse Reitz <jessereitz1@gmail.com>
2018-10-31 23:57:18 -0500

    Run gnome setup as user instead of sudo.

--- init-ubuntu.sh
+++ init-ubuntu.sh
@@ -9,47 +9,47 @@ add-apt-repository multiverse
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
 apt update
-echo "\n\nUpgrading packages."
+echo -e "\n\nUpgrading packages."
 apt --assume-yes upgrade
 sudo apt-get -y install google-chrome-stable
 
 # Install git
-echo "\n\nInstalling Git."
+echo -e "\n\nInstalling Git."
 apt install --assume-yes git
 git config --global user.name "Jesse Reitz"
 git config --global user.email "jessereitz1@gmail.com"
 
 # Install Gnome-Tweaks and set up Gnome
-echo "\n\nInstalling Gnome-Tweaks. Setting up Gnome preferences."
+echo -e "\n\nInstalling Gnome-Tweaks. Setting up Gnome preferences."
 apt install --assume-yes gnome-tweak-tool
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
-gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
+-H -u $SUDO_USER bash -c 'gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM'
+-H -u $SUDO_USER bash -c 'gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true'
 
 
 # Install text editors and their configurations
-echo "\n\nInstalling Vim, vimrc."
+echo -e "\n\nInstalling Vim, vimrc."
 apt install --assume-yes vim
 git clone https://github.com/jessereitz/vimrc.git ~/.vim
-echo "\n\nInstalling Atom."
+echo -e "\n\nInstalling Atom."
 snap install --classic atom
 apm install --assume-yes sync-settings
 
 # Install and set up Python and Python packages
-echo "\n\nInstalling python3-pip, virtualenv."
+echo -e "\n\nInstalling python3-pip, virtualenv."
 apt install --assume-yes python3-pip
 pip3 install virtualenv
 
 # Install Node
-echo "\n\nInstalling Node, NPM."
+echo -e "\n\nInstalling Node, NPM."
 apt install --assume-yes nodejs npm
 yes | npm install -g npm@latest
 # Clear the npm cache to use updated npm version
 yes | npm cache clean --force
 
 # Install Node packages
-echo "\n\nInstalling Node packages: http-server, eslint, sass"
+echo -e "\n\nInstalling Node packages: http-server, eslint, sass"
 yes | npm install -g http-server eslint sass
 
-echo "\n\nAll packages installed. Don't forget to set up Atom Sync-Settings."
+echo -e "\n\nAll packages installed. Don't forget to set up Atom Sync-Settings."
 
 rm ~/init-ubuntu.sh
